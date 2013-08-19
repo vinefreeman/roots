a2dd40c83a184c7dafd32858ebb4db401f645450
vinefreeman <vinny.redhead@gmail.com>
2013-08-19 18:06:07 +0100

    new home template start

---
+++ templates/content-home.php
@@ -0,0 +1,14 @@
+<!--<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?> 
+Awaiting theme inclusion - custom fields etc.
+-->
+<!--=======================================
+Home Template
+===========================================-->
+<div class="row dark">
+	<div class="col-lg-4 col-md-4 col-sm-4">Box 1</div>
+	<div class="col-lg-4 col-md-4 col-sm-4">Box 2</div>
+	<div class="col-lg-4 col-md-4 col-sm-4">Box 3</div>
+</div>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,7 @@
+<?php if (is_home() || is_front_page() ): else: ;?>
 <div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
+  	<h1>
+	    <?php echo roots_title(); ?>
+	</h1>
 </div>
+<?php endif; ?>
