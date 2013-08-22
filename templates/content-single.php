2e5fd775213c0bd9511c8c7cdf029dc6a8ab0ac0
vinefreeman <vinny.redhead@gmail.com>
2013-08-22 18:45:22 +0100

    blog template changes

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,9 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
+      <div class="page-header">
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
+    </div>
     </header>
+    <div class="innerspace">
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
@@ -11,5 +14,6 @@
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
+  </div>
   </article>
 <?php endwhile; ?>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,7 +1,14 @@
-<?php if (is_home() || is_front_page() ): else: ;?>
+<?php if (is_front_page() ): else: ;?>
 <div class="page-header">
   	<h1>
 	    <?php echo roots_title(); ?>
 	</h1>
+	<?php if (is_home()){
+				
+				if(get_post_meta( 16, '_nicetitle', true )){
+					echo "<span class='nicetitle'>" . get_post_meta( 16, '_nicetitle', true) . "</span>";
+				}
+			
+	} ?>
 </div>
 <?php endif; ?>
