7152cafe51e4f224864a476fc4b1f1bc38dbc076
vinefreeman <vinny.redhead@gmail.com>
2013-08-22 13:22:38 +0100

    Template additions and changes:  sidebar, page custom

---
+++ template-innercol.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Page with Inner Column
+*/
+?>
+
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'inner'); ?>
---
+++ templates/content-inner.php
@@ -0,0 +1,13 @@
+<div class="innerspace"><!-- white background container -->
+	<div class="row">
+		<div class="col-sm-12 col-md-8 col-lg-9">
+			<?php while (have_posts()) : the_post(); ?>
+			  <?php the_content(); ?>
+			  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+			<?php endwhile; ?>
+		</div>
+		<div class="col-sm-12 col-md-4 col-lg-3">
+			<?php dynamic_sidebar('sidebar-inner'); ?>
+		</div>
+	</div>
+</div>
