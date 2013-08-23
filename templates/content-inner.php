56500a212666e851a30d03dffab4399035621980
vinefreeman <vinny.redhead@gmail.com>
2013-08-23 15:21:11 +0100

    innerside styling

--- templates/content-inner.php
+++ templates/content-inner.php
@@ -1,6 +1,6 @@
 <div class="innerspace"><!-- white background container -->
 	<div class="row">
-		<div class="col-sm-12 col-md-8 col-lg-9">
+		<div class="col-sm-12 col-md-9 col-lg-9">
 			<?php 
 				if(get_post_meta( $post->ID, '_nicetitle', true )){
 					echo "<span class='nicetitle'>" . get_post_meta( $post->ID, '_nicetitle', true) . "</span>";
@@ -11,8 +11,9 @@
 			  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 			<?php endwhile; ?>
 		</div>
-		<div class="col-sm-12 col-md-4 col-lg-3">
+		<div class="col-sm-12 col-md-3 col-lg-3">
 			<?php dynamic_sidebar('sidebar-inner'); ?>
+			
 		</div>
 	</div>
 </div>
