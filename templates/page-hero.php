d0e8815ced07bb80517e5351047e5dcb3483454f
vinefreeman <vinny.redhead@gmail.com>
2013-10-15 14:44:57 +0100

    CSS and carousel changes

--- templates/page-hero.php
+++ templates/page-hero.php
@@ -8,33 +8,32 @@
          
         <div class="active item">
             <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/slide1.jpg" class="hero img-responsive">
-              <div class="carousel-caption"><h2><span>Leading the Way</span></h2><p>Developing internally, we endeavour to progress and build upon our successes...</p></div>
+              <div class="carousel-caption"><h2><span>Leading the Way</span></h2><p><em>Developing internally, we endeavour to progress and build upon our successes...</em></p></div>
         </div>
         <div class="item">
             <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/slide2.jpg" class="hero img-responsive">
-            <div class="carousel-caption"><h2><span>Behind the scenes</span></h2><p>Developing internally, we endeavour to progress and build upon our successes...</p></div>
+            <div class="carousel-caption"><h2><span>Behind the scenes</span></h2><p><em>Developing internally, we endeavour to progress and build upon our successes...</em></p></div>
          </div>
          <div class="item">
             <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/slide3.jpg" class="hero img-responsive">
-            <div class="carousel-caption"><h2><span>A closer look</span></h2><p>Developing internally, we endeavour to progress and build upon our successes...</p></div>
+            <div class="carousel-caption"><h2><span>A closer look</span></h2><p><em>Developing internally, we endeavour to progress and build upon our successes...</em></p></div>
          </div>
           <div class="item">
             <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/slide4.jpg" class="hero img-responsive">
-            <div class="carousel-caption"><h2><span>Securing London against corporate threat</span></h2><p>Developing internally, we endeavour to progress and build upon our successes...</p></div>
+            <div class="carousel-caption"><h2><span>Securing London against corporate threat</span></h2><p><em>Developing internally, we endeavour to progress and build upon our successes...</em></p></div>
          </div>
-
       </div>
-        <!-- Carousel nav needs styling -->
-       <!-- <ol class="carousel-indicators">
+        <!-- Carousel nav needs styling --> 
+          <ol class="carousel-indicators hidden-xs">
               <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
               <li data-target="#myCarousel" data-slide-to="1"></li>
               <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>-->
-         
+              <li data-target="#myCarousel" data-slide-to="3"></li>
+           </ol>     
           
           <!-- nav controls here -->
-        <a class="carousel-control left hidden-xs" href="#myCarousel" data-slide="prev"><img src="<?php bloginfo('stylesheet_directory');?>/assets/img/left-arr.gif"></a>
-           <a class="carousel-control right hidden-xs" href="#myCarousel" data-slide="next"><img src="<?php bloginfo('stylesheet_directory');?>/assets/img/right-arr.gif"></a>
+        <!--<a class="carousel-control left hidden-xs" href="#myCarousel" data-slide="prev"><img src="<?php bloginfo('stylesheet_directory');?>/assets/img/left-arr.gif"></a>
+           <a class="carousel-control right hidden-xs" href="#myCarousel" data-slide="next"><img src="<?php bloginfo('stylesheet_directory');?>/assets/img/right-arr.gif"></a>-->
          
      
         <!--<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
