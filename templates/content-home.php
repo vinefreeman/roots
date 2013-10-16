bb14cc48d0c6d560bb8cd6c362ea61e84ba9a869
vinefreeman <vinny.redhead@gmail.com>
2013-10-16 10:39:57 +0100

    home page

--- templates/content-home.php
+++ templates/content-home.php
@@ -9,7 +9,7 @@ Home Template
 ===========================================-->
 <div class="row dark feature">
 	<div class="col-lg-4 col-md-4 col-sm-4 clearfix">
-		<a href="#"><img src="<?php bloginfo('stylesheet_directory');?>/assets/img/feature-hold-img.jpg" class="img-responsive"></a>
+		<a href="#" data-toggle="tooltip" title="Some of our excellent work"><img src="<?php bloginfo('stylesheet_directory');?>/assets/img/feature-hold-img.jpg" class="img-responsive"></a>
 		<h2>Current ACS Score.</h2>
 		<p>Our score is rising constantly and has been for years, what does this mean for you?</p>
 		<a href="#" class="btn btn-dark">read more</a>
