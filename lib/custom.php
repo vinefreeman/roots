6d52ff0f7d1501eae2cdd5eed28cb87eb2d9ca86
vinefreeman <vinny.redhead@gmail.com>
2013-10-28 14:41:40 +0000

    custom post and metabox working

--- lib/custom.php
+++ lib/custom.php
@@ -82,9 +82,9 @@ function myplugin_save_postdata( $post_id ) {
    #allow adding of shortcodes to text     
    add_filter('widget_text', 'do_shortcode');
 
-
-  #Custom post type test
-
+  #================================================================
+  #               Custom post type job
+  #================================================================
   #first create custom post type with labels and related info 
   function _custom_post_job() {
     $labels = array(
@@ -154,7 +154,7 @@ function change_default_title( $title ){
      $screen = get_current_screen();
  
      if  ( 'cisjob' == $screen->post_type ) {
-          $title = 'Enter Job Title Here';
+          $title = 'Enter job title here - then the description below';
      }
  
      return $title;
@@ -230,5 +230,122 @@ function my_map_meta_cap( $caps, $cap, $user_id, $args ) {
   /* Return the capabilities required by the user. */
   return $caps;
 }
+
+
+/* Fire our meta box setup function on the post editor screen. */
+add_action( 'load-post.php', 'cis_job_box_setup' );
+add_action( 'load-post-new.php', 'cis_job_box_setup' );
+
+/* Meta box setup function. */
+function cis_job_box_setup() {
+
+  /* Add meta boxes on the 'add_meta_boxes' hook. */
+  add_action( 'add_meta_boxes', 'cisjob_box' );
+
+  /* Save post meta on the 'save_post' hook. */
+  add_action( 'save_post', 'cisjob_box_save', 10, 2 );
+}
+
+
+# Metabox for post
+function cisjob_box() {
+    add_meta_box( 
+        'cisjob_box',
+        __( 'Vacancy Details', 'example' ),
+        'cisjob_box_content',
+        'cisjob',
+        'normal',
+        'high'
+    );
+}
+
+/* Display the post meta box. */
+function cisjob_box_content( $object ) { ?>
+
+  <?php wp_nonce_field( basename( __FILE__ ), 'cisjob_box_nonce' ); ?>
+
+  <p>
+    <label for="job_client"><?php _e( "Client/Location", 'example' ); ?></label>
+    <br />
+    <input class="widefat" type="text" name="job_client" id="job_client" value="<?php echo esc_attr( get_post_meta( $object->ID, 'job_client', true ) ); ?>" size="30" />
+  </p>
+  <p>
+    <label for="job_pay"><?php _e( "Pay Rate", 'example' ); ?></label>
+    <br />
+    <input class="widefat" type="text" name="job_pay" id="job_pay" value="<?php echo esc_attr( get_post_meta( $object->ID, 'job_pay', true ) ); ?>" size="30" />
+  </p>
+   <p>
+    <label for="job_shift"><?php _e( "Shift Details", 'example' ); ?></label>
+    <br />
+    <input class="widefat" type="text" name="job_shift" id="job_shift" value="<?php echo esc_attr( get_post_meta( $object->ID, 'job_shift', true ) ); ?>" size="30" />
+  </p>
+  <p>
+    <label for="job_ref"><?php _e( "Job Reference", 'example' ); ?></label>
+    <br />
+    <input class="widefat" type="text" name="job_ref" id="job_ref" value="<?php echo esc_attr( get_post_meta( $object->ID, 'job_ref', true ) ); ?>" size="30" />
+  </p>
+ 
+<?php }
+
+/* Save the meta box's post metadata. */
+function cisjob_box_save( $post_id, $post) {
+
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+  return;
+
+  /* Verify the nonce before proceeding. */
+  if ( !isset( $_POST['cisjob_box_nonce'] ) || !wp_verify_nonce( $_POST['cisjob_box_nonce'], basename( __FILE__ ) ) )
+    return $post_id;
+
+  /* Get the post type object. */
+  $post_type = get_post_type_object( $post->post_type );
+
+  /* Check if the current user has permission to edit the post. */
+  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+    return $post_id;
+
+  /* Get the posted data and sanitize it text. */
+  $new_client_value = ( isset( $_POST['job_client'] ) ? sanitize_text_field( $_POST['job_client'] ) : '' );
+  /* Get the meta key. */
+  $meta_client_key = 'job_client';
+  /*  Update meta on post - VF*/
+  update_post_meta( $post_id, $meta_client_key, $new_client_value ); 
+
+  /* Pay Rate*/
+  $new_pay_value = ( isset( $_POST['job_pay'] ) ? sanitize_text_field( $_POST['job_pay'] ) : '' );
+  $meta_pay_key = 'job_pay';
+  update_post_meta( $post_id, $meta_pay_key, $new_pay_value ); 
+
+   /* Shift info*/
+  $new_shift_value = ( isset( $_POST['job_shift'] ) ? sanitize_text_field( $_POST['job_shift'] ) : '' );
+  $meta_shift_key = 'job_shift';
+  update_post_meta( $post_id, $meta_shift_key, $new_shift_value );
+
+   /* Job Ref*/
+  $new_ref_value = ( isset( $_POST['job_ref'] ) ? sanitize_text_field( $_POST['job_ref'] ) : '' );
+  $meta_ref_key = 'job_ref';
+  update_post_meta( $post_id, $meta_ref_key, $new_ref_value ); 
+
+
+  /* Get the meta value of the custom field key. 
+  $meta_client_value = get_post_meta( $post_id, $meta_client_key, true );*/
+
+  /* If a new meta value was added and there was no previous value, add it.
+  if ( $new_client_value && '' == $meta_client_value )
+    add_post_meta( $post_id, $meta_client_key, $new_client_value, true ); */
+
+  /* If the new meta value does not match the old value, update it.
+  elseif ( $new_client_value && $new_client_value != $meta_client_value )
+    update_post_meta( $post_id, $meta_client_key, $new_client_value ); */
+
+  /* If there is no new meta value but an old value exists, delete it. 
+  elseif ( '' == $new_client_value && $meta_client_value )
+    delete_post_meta( $post_id, $meta_client_key, $meta_client_value );*/
+}
+
+
+#================================================================
+#               End custom post type job
+#================================================================
 
 ?>
