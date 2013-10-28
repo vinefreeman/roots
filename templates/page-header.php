a4b6c8c3fb7a3ef56096270479b3eda6ff98fefc
vinefreeman <vinny.redhead@gmail.com>
2013-10-28 17:02:58 +0000

    custom templates and posts

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,8 +1,13 @@
 <?php if (is_front_page() ): else: ;?>
 <div class="page-header">
-  	<h1>
-	    <?php echo roots_title(); ?>
-	</h1>
+	<?php // show job count
+	if (is_post_type_archive( 'cisjob' )){
+		$count_posts = wp_count_posts('cisjob'); 
+		$jobs = $count_posts->publish;
+		$showme = true;
+	}
+	?>
+  	<h1><?php echo roots_title(); ?><?php if ($showme){echo "<span class='badge'>".$jobs."</span>";} ?></h1> 
 	<?php if (is_home()){
 				
 				if(get_post_meta( 16, '_nicetitle', true )){
