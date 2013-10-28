89c57798b4670a781916d5d6b6c21c236cd2ea2f
Vinny <vinny.redhead@gmail.com>
2013-10-28 11:22:40 +0000

    cust post additions

--- lib/custom.php
+++ lib/custom.php
@@ -185,4 +185,50 @@ function my_taxonomies_job() {
 }
 add_action( 'init', 'my_taxonomies_job', 0 );
 
+#capabilities meta
+
+add_filter( 'map_meta_cap', 'my_map_meta_cap', 10, 4 );
+
+function my_map_meta_cap( $caps, $cap, $user_id, $args ) {
+
+  /* If editing, deleting, or reading a movie, get the post and post type object. */
+  if ( 'edit_cisjob' == $cap || 'delete_cisjob' == $cap || 'read_cisjob' == $cap ) {
+    $post = get_post( $args[0] );
+    $post_type = get_post_type_object( $post->post_type );
+
+    /* Set an empty array for the caps. */
+    $caps = array();
+  }
+
+  /* If editing a movie, assign the required capability. */
+  if ( 'edit_cisjob' == $cap ) {
+    if ( $user_id == $post->post_author )
+      $caps[] = $post_type->cap->edit_posts;
+    else
+      $caps[] = $post_type->cap->edit_others_posts;
+  }
+
+  /* If deleting a movie, assign the required capability. */
+  elseif ( 'delete_cisjob' == $cap ) {
+    if ( $user_id == $post->post_author )
+      $caps[] = $post_type->cap->delete_posts;
+    else
+      $caps[] = $post_type->cap->delete_others_posts;
+  }
+
+  /* If reading a private cisjob, assign the required capability. */
+  elseif ( 'read_cisjob' == $cap ) {
+
+    if ( 'private' != $post->post_status )
+      $caps[] = 'read';
+    elseif ( $user_id == $post->post_author )
+      $caps[] = 'read';
+    else
+      $caps[] = $post_type->cap->read_private_posts;
+  }
+
+  /* Return the capabilities required by the user. */
+  return $caps;
+}
+
 ?>
